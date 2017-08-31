2025f7a225814b43af9648493dc2831a76cd146b
Deven <devensocial@gmail.com>
2017-09-01 02:16:31 +0530

    Update securecodeA.c

--- securecodeA.c
+++ securecodeA.c
@@ -5,7 +5,7 @@ int main()
 {
 	char word1[10];
 	char word2[10];
-	int i=0,j;
+	int i=0,j,index;
 	FILE *co;
 	co = FILE fopen("code.txt","r");  //code file
 	if (co==NULL)
@@ -29,7 +29,7 @@ int main()
 			word1[i]=getch(co);
 			i++;
 		}
-		j=word[0];				//calculate ASCII value of the first alphabet and then start the search accordingly
+		j=word[0];						//calculate ASCII value of the first alphabet and then start the search accordingly
 		while(getch(ch2)=="%")			//how the keywords alphabet list will end
 		{
 			j=0;
@@ -38,6 +38,13 @@ int main()
 				word2[j]=getch(sr);		//implement dictionary search here
 				j++;
 			}
+		}
+		if(word1==word2)
+		{
+			printf("Your code might have some vulnerability: \n");
+			index=word2[0];				//for seekp to print the report
+			fseek( sr, index, SEEK_SET );
+			getch(co)					//implement seekp
 		}
 	}
 }
