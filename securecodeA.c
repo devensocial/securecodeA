c80b91258c01317cfb814f5a971e86b9913031e4
Deven <devensocial@gmail.com>
2017-10-31 02:38:42 +0530

    Update securecodeA.c

--- securecodeA.c
+++ securecodeA.c
@@ -1,27 +1,16 @@
 #include<stdio.h>
 #include<string.h>
+#include<conio.h>
 #include<stiod.h> //for fgets()
-int substring(char a[100], char b["p","r","e","g","_","r","e","p","l","a","c","e"])
-{
-	char ss[20];
-	ss = strstr(a,b);
-	return strcmp(b,ss);
-}
 int main()
 {
 	char word1[100];
 	char word2[10];
 	int choice;
 	int i=0,j,index;
-	FILE *co;
-	char ch1,ch2;
-	ch1=NULL;						//for fetching chars
-	FILE *sr;
-	const char EOL = "//0";
-	char *buffer;
-	size_t bufsize = 32;
-	size_t characters;
-	buffer = (char *)malloc(bufsize * sizeof(char));		//dynamic allocation of char for getline
+	FILE *co;						//for fetching chars
+	//FILE *sr;
+	const char EOL = "/0";
 	printf("What file type you want to scan for analysis?\n1.C\n2.PHP");
 	scanf("%d",&choice)
 	if (choice == 1)
@@ -57,7 +46,14 @@ int main()
 	
 	while(!feof(co))
 	{
-		word1[] = co.sgets(&buffer,&bufsize);
+		i++;
+		while (ch!=" "|| ch!="("){
+		ch=getc(co);
+		word1[i]=ch;
+			i++
+		}
+		
+		
 		if(word1 == "String" || word1 == "Query" || word1 == "1'='1" )
 		{
 			printf("\n");
@@ -67,7 +63,7 @@ int main()
 		{
 			printf("\nThis function is vulnerable to BufferOverflow error as in the input, if it recives the input as a pointer, then it couldn't estimate the size of it.\n")
 		}
-		else if(substring(word1,"preg_replace")==1)
+		else if(strstr(word1,"preg_replace ")==1)
 		{
 			printf("\n0");
 		}
@@ -75,7 +71,7 @@ int main()
 		{
 			printf("\n");
 		}
-		else if( word1 == "strcpy()")
+		else if( word1 == "strcpy()" || word1 == "strcpy" ) || word1 == "strcpy(")
 		{
 			printf("\nYour code might have some vulnerablities and this can better be heal;ed by replacing this function by strncpy().\n");
 		}
@@ -83,13 +79,45 @@ int main()
 		{
 			printf("\n");
 		}
-		else if( word1 == "")
-	}
-	while(fscanf(co,"%s",a )
-	{
-		word1 = co.fgets()
-		{	
+		else if( word1 == "strcpyA")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "strcpyW")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "StrCpyNA")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "StrCpyNW")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "StrNCpyA")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "StrNCpyW")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "StrNCpy")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "strcpynA")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "strcpyn")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
+		}
+		else if( word1 == "strcpyn(")
+		{
+			printf("\nFunction appears in Microsoft's banned function list. Can facilitate buffer overflow conditions.");
 		}
 	}
-
 }
